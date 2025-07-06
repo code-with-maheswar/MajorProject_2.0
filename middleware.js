9480cdaddd1691c4882e9593519548e1745f7d56
Maheswar manna <mannamaheshwar51@gmail.com>
2025-07-06 22:09:28 +0530

    new update

--- middleware.js
+++ middleware.js
@@ -8,9 +8,9 @@ module.exports.isLoggedIn=(req,res,next)=>{
     if (req.isAuthenticated && req.isAuthenticated()) {
   req.session.redirectUrl=req.originalUrl;
     req.flash("error","You must be logged in");
-  return  next();
+  return res.redirect("/login");
   }
-res.redirect("/login");
+  next();
 }
 
 module.exports.saveRedirectUrl = (req,res,next)=>
